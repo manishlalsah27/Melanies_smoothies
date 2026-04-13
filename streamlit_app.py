940c8989c8dbdf7f4dc4e05e4c962648bd71e5e5
manishlalsah27 <manishlalsah27@gmail.com>
2026-04-13 18:23:24 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,7 @@ import streamlit as st
 import requests
 from snowflake.snowpark.functions import col
 
-st.title("Debug Secrets")
 
-st.write(st.secrets)
 
 st.title("Customize Your Smoothie :cup_with_straw:")
 st.write("Choose the fruits you want in your custom Smoothie!")
